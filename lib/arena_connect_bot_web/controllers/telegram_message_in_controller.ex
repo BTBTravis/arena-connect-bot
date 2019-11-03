0fd837fbb08d78ae458da59e6d0f6a9cecdd5aba
Travis Shears <btbtravis@gmail.com>
2019-11-03 18:22:17 +0100

    get chat_id from message

--- lib/arena_connect_bot_web/controllers/telegram_message_in_controller.ex
+++ lib/arena_connect_bot_web/controllers/telegram_message_in_controller.ex
@@ -8,24 +8,16 @@ defmodule ArenaConnectBotWeb.TelegramMessageInController do
     |> json(%{reason: msg})
   end
 
+  def get_chat_id(post_data) do
+    get_in(post_data, ["message", "chat", "id"])
+  end
+
   # method to handle all incoming telegram messages
   def index(conn, params) do
-    case Map.fetch(params, :message) do
-      {:ok, msg} -> json(conn, %{handled: true, msg: msg})
-      :error -> send400(conn, "no message found")
-    end
-    #Map.fetch(params, :message)
-    #case Poison.decode!(params, as: %ArenaConnectBot.IncomingMessage{}) do
-      #{:ok, msg}      -> IO.puts "decoded: #{msg}"
-      #{:error, reason} -> IO.puts "Error"
-    #end
-    #msg = Poison.decode(params, as: %ArenaConnectBot.IncomingMessage{})
     #IO.inspect(params)
-    # todo decode message and if it is not message return bad status 400
-    #IO.inspect params
-    #text(conn, Poison.encode!(%ArenaConnectBot.IncomingMessage{message_id: 88, from_id: 111, chat_id: 222, text: "hello world"}))
-    #json(conn, %ArenaConnectBot.IncomingMessage{message_id: 88, from_id: 111, chat_id: 222, text: "hello world"})
-    #json(conn, %{handled: true})
-    #render(conn, "index.html")
+    case get_chat_id(params) do
+      nil -> send400(conn, "no message found")
+      chat_id -> json(conn, %{handled: true, chat_id: chat_id})
+    end
   end
 end
--- test/arena_connect_bot_web/controllers/telegram_message_in_controller_test.exs
+++ test/arena_connect_bot_web/controllers/telegram_message_in_controller_test.exs
@@ -5,51 +5,37 @@ defmodule ArenaConnectBotWeb.TelegramMessageInControllerTest do
         msg = %{
           "message" => nil
         }
-            #"chat" => %{
-              #"first_name" => "Travis",
-              #"id" => 953058570,
-              #"last_name" => "Shears",
-              #"type" => "private"
-            #},
-            #"date" => 1572799466,
-            #"entities" => [%{"length" => 47, "offset" => 0, "type" => "url"}],
-            #"from" => %{
-              #"first_name" => "Travis",
-              #"id" => 953058570,
-              #"is_bot" => false,
-              #"language_code" => "en",
-              #"last_name" => "Shears"
-            #},
-            #"message_id" => 3,
-            #"text" => "https://hexdocs.pm/phoenix/testing.html#content"
-          #},
-          #"update_id" => 217220855
-        #}
       conn = post(conn, "/api/message", msg)
       assert json_response(conn, 400)
       #assert json_response(conn, 200) == %{"handled" => true}
     end
+
+    test "/api/message accepts valid message", %{conn: conn} do
+      msg = %{
+        "message" => %{
+          "chat" => %{
+            "first_name" => "Travis",
+            "id" => 953058570,
+            "last_name" => "Shears",
+            "type" => "private"
+          },
+          "date" => 1572799466,
+          "entities" => [%{"length" => 47, "offset" => 0, "type" => "url"}],
+          "from" => %{
+            "first_name" => "Travis",
+            "id" => 953058570,
+            "is_bot" => false,
+            "language_code" => "en",
+            "last_name" => "Shears"
+          },
+          "message_id" => 3,
+          "text" => "https://hexdocs.pm/phoenix/testing.html#content"
+        },
+        "update_id" => 217220855
+      }
+      conn = post(conn, "/api/message", msg)
+      #assert json_response(conn, 400)
+      assert json_response(conn, 200) == %{"handled" => true, "chat_id" => 953058570}
+    end
 end
 
-        #msg = %{
-          #"message" => %{
-            #"chat" => %{
-              #"first_name" => "Travis",
-              #"id" => 953058570,
-              #"last_name" => "Shears",
-              #"type" => "private"
-            #},
-            #"date" => 1572799466,
-            #"entities" => [%{"length" => 47, "offset" => 0, "type" => "url"}],
-            #"from" => %{
-              #"first_name" => "Travis",
-              #"id" => 953058570,
-              #"is_bot" => false,
-              #"language_code" => "en",
-              #"last_name" => "Shears"
-            #},
-            #"message_id" => 3,
-            #"text" => "https://hexdocs.pm/phoenix/testing.html#content"
-          #},
-          #"update_id" => 217220855
-        #}
