4025e3eccc1ede49de049f46f871a152cffb21cb
Travis Shears <btbtravis@gmail.com>
2019-11-03 18:04:36 +0100

    400 on /api/message post without msg

--- lib/arena_connect_bot_web/controllers/telegram_message_in_controller.ex
+++ lib/arena_connect_bot_web/controllers/telegram_message_in_controller.ex
@@ -2,19 +2,30 @@ defmodule ArenaConnectBotWeb.TelegramMessageInController do
   use ArenaConnectBotWeb, :controller
   #use ArenaConnectBot.IncomingMessage
 
+  def send400(conn, msg) do
+    conn
+    |> put_status(400)
+    |> json(%{reason: msg})
+  end
+
   # method to handle all incoming telegram messages
   def index(conn, params) do
+    case Map.fetch(params, :message) do
+      {:ok, msg} -> json(conn, %{handled: true, msg: msg})
+      :error -> send400(conn, "no message found")
+    end
+    #Map.fetch(params, :message)
     #case Poison.decode!(params, as: %ArenaConnectBot.IncomingMessage{}) do
       #{:ok, msg}      -> IO.puts "decoded: #{msg}"
       #{:error, reason} -> IO.puts "Error"
     #end
     #msg = Poison.decode(params, as: %ArenaConnectBot.IncomingMessage{})
-    IO.inspect(params)
+    #IO.inspect(params)
     # todo decode message and if it is not message return bad status 400
     #IO.inspect params
     #text(conn, Poison.encode!(%ArenaConnectBot.IncomingMessage{message_id: 88, from_id: 111, chat_id: 222, text: "hello world"}))
     #json(conn, %ArenaConnectBot.IncomingMessage{message_id: 88, from_id: 111, chat_id: 222, text: "hello world"})
-    json(conn, %{handled: true})
+    #json(conn, %{handled: true})
     #render(conn, "index.html")
   end
 end
--- test/arena_connect_bot_web/controllers/telegram_message_in_controller_test.exs
+++ test/arena_connect_bot_web/controllers/telegram_message_in_controller_test.exs
@@ -1,8 +1,55 @@
 defmodule ArenaConnectBotWeb.TelegramMessageInControllerTest do
     use ArenaConnectBotWeb.ConnCase
 
-    test "POST /api/message", %{conn: conn} do
-          conn = post(conn, "/api/message")
-          assert json_response(conn, 200) == %{"handled" => true}
+    test "/api/message only accepts valid messages", %{conn: conn} do
+        msg = %{
+          "message" => nil
+        }
+            #"chat" => %{
+              #"first_name" => "Travis",
+              #"id" => 953058570,
+              #"last_name" => "Shears",
+              #"type" => "private"
+            #},
+            #"date" => 1572799466,
+            #"entities" => [%{"length" => 47, "offset" => 0, "type" => "url"}],
+            #"from" => %{
+              #"first_name" => "Travis",
+              #"id" => 953058570,
+              #"is_bot" => false,
+              #"language_code" => "en",
+              #"last_name" => "Shears"
+            #},
+            #"message_id" => 3,
+            #"text" => "https://hexdocs.pm/phoenix/testing.html#content"
+          #},
+          #"update_id" => 217220855
+        #}
+      conn = post(conn, "/api/message", msg)
+      assert json_response(conn, 400)
+      #assert json_response(conn, 200) == %{"handled" => true}
     end
 end
+
+        #msg = %{
+          #"message" => %{
+            #"chat" => %{
+              #"first_name" => "Travis",
+              #"id" => 953058570,
+              #"last_name" => "Shears",
+              #"type" => "private"
+            #},
+            #"date" => 1572799466,
+            #"entities" => [%{"length" => 47, "offset" => 0, "type" => "url"}],
+            #"from" => %{
+              #"first_name" => "Travis",
+              #"id" => 953058570,
+              #"is_bot" => false,
+              #"language_code" => "en",
+              #"last_name" => "Shears"
+            #},
+            #"message_id" => 3,
+            #"text" => "https://hexdocs.pm/phoenix/testing.html#content"
+          #},
+          #"update_id" => 217220855
+        #}
