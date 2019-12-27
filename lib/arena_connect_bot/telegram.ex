dde027f07e95f97536012e638ebcedc7dfaf5e40
Travis Shears <btbtravis@gmail.com>
2019-12-27 12:36:50 +0000

    init user id checking

--- lib/arena_connect_bot/telegram.ex
+++ lib/arena_connect_bot/telegram.ex
@@ -5,6 +5,10 @@ defmodule ArenaConnectBot.Telegram do
     Application.get_env(:arena_connect_bot, ArenaConnectBot.Telegram.Api)[:token]
   end
 
+  def get_admin_user_id() do
+    Application.get_env(:arena_connect_bot, ArenaConnectBot.Telegram.Api)[:admin_user_id]
+  end
+
   def send_message_to_chat(chat_id, msg) do
     Api.client(get_token())
     |> Api.send_msg(chat_id, msg)
